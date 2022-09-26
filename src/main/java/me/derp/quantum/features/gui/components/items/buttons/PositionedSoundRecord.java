113f419324e1c81d67a02b4598714bd7830adf4e
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2022-09-27 00:19:22 +0200

    Add files via upload

---
+++ src/main/java/me/derp/quantum/features/gui/components/items/buttons/PositionedSoundRecord.java
@@ -0,0 +1,9 @@
+package me.derp.quantum.features.gui.components.items.buttons;
+
+public class PositionedSoundRecord {
+
+    public static Object getMasterRecord(SoundEvent ui_BUTTON_CLICK, float f) {
+        return null;
+    }
+
+}
