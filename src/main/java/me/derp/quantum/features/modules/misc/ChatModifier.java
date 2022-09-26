cc3e709bb98bca563d389d65b306e57a5932c5f6
ZANE <88916121+Zane2b2t@users.noreply.github.com>
2022-09-26 17:19:27 +0200

    Removed small bloat from chat suffix

--- src/main/java/me/derp/quantum/features/modules/misc/ChatModifier.java
+++ src/main/java/me/derp/quantum/features/modules/misc/ChatModifier.java
@@ -1,66 +1,49 @@
 package me.derp.quantum.features.modules.misc;
 
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.network.play.client.CPacketChatMessage;
 import me.derp.quantum.event.events.PacketEvent;
-import me.derp.quantum.features.command.Command;
-import me.derp.quantum.features.modules.Module;
 import me.derp.quantum.features.setting.Setting;
 import me.derp.quantum.util.Timer;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.network.play.client.CPacketChatMessage;
-import net.minecraft.util.math.Vec3i;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import me.derp.quantum.features.modules.Module;
 
-public class ChatModifier
-        extends Module {
-    public Setting<Suffix> suffix = this.register(new Setting<>("Suffix", Suffix.NONE, "Your Suffix."));
-    public Setting<Boolean> clean = this.register(new Setting<>("CleanChat", Boolean.FALSE, "Cleans your chat"));
-    public Setting<Boolean> infinite = this.register(new Setting<>("Infinite", Boolean.FALSE, "Makes your chat infinite."));
-    public Setting<Boolean> autoQMain = this.register(new Setting<>("AutoQMain", Boolean.FALSE, "Spams AutoQMain"));
-    public Setting<Boolean> qNotification = this.register(new Setting<Object>("QNotification", Boolean.FALSE, v -> this.autoQMain.getValue()));
-    public Setting<Integer> qDelay = this.register(new Setting<Object>("QDelay", 9, 1, 90, v -> this.autoQMain.getValue()));
-    private final Timer timer = new Timer();
-    private static ChatModifier INSTANCE = new ChatModifier();
-
+public class ChatModifier extends Module
+{
+    private static ChatModifier INSTANCE;
+    private final Timer timer;
+    public Setting<Suffix> suffix;
+    
     public ChatModifier() {
-        super("Chat", "Modifies your chat", Module.Category.MISC, true, false, false);
+        super("ChatSuffix", "Modifies your chat", Category.MISC, true, false, false);
+        this.timer = new Timer();
+        this.suffix = (Setting<Suffix>)this.register(new Setting("Suffix", (T)Suffix.MCDONALDS, "Your Suffix."));
         this.setInstance();
     }
-
-    private void setInstance() {
-        INSTANCE = this;
-    }
-
+    
     public static ChatModifier getInstance() {
-        if (INSTANCE == null) {
-            INSTANCE = new ChatModifier();
+        if (ChatModifier.INSTANCE == null) {
+            ChatModifier.INSTANCE = new ChatModifier();
         }
-        return INSTANCE;
+        return ChatModifier.INSTANCE;
     }
-
-    @Override
-    public void onUpdate() {
-        if (this.autoQMain.getValue()) {
-            if (!this.shouldSendMessage(ChatModifier.mc.player)) {
-                return;
-            }
-            if (this.qNotification.getValue()) {
-                Command.sendMessage("<AutoQueueMain> Sending message: /queue main");
-            }
-            ChatModifier.mc.player.sendChatMessage("/queue main");
-            this.timer.reset();
-        }
+    
+    private void setInstance() {
+        ChatModifier.INSTANCE = this;
     }
-
+    
     @SubscribeEvent
-    public void onPacketSend(PacketEvent.Send event) {
+    public void onPacketSend(final PacketEvent.Send event) {
         if (event.getStage() == 0 && event.getPacket() instanceof CPacketChatMessage) {
-            CPacketChatMessage packet = event.getPacket();
+            final CPacketChatMessage packet = event.getPacket();
             String s = packet.getMessage();
             if (s.startsWith("/")) {
                 return;
             }
-            if (this.suffix.getValue() == Suffix.Quantum) {
-                s = s + " \u23d0 \uff51\uff55\uff41\uff4e\uff54\uff55\uff4d\u3000 \uff43\uff4f\uff4e\uff54\uff49\uff4e\uff55\uff45\uff44";
+            switch (this.suffix.getValue()) {
+                case MCDONALDS: {
+                    s += " \u23d0 \u1d0d\u1d04\u1d05\u1d0f\u0274\u1d00\u029f\u1d05\ua731 \u1d04\u029f\u026a\u1d07\u0274\u1d1b";
+                    break;
+                }
             }
             if (s.length() >= 256) {
                 s = s.substring(0, 256);
@@ -68,22 +51,13 @@ public class ChatModifier
             packet.message = s;
         }
     }
-
-
-    private boolean shouldSendMessage(EntityPlayer player) {
-        if (player.dimension != 1) {
-            return false;
-        }
-        if (!this.timer.passedS(this.qDelay.getValue())) {
-            return false;
-        }
-        return player.getPosition().equals(new Vec3i(0, 240, 0));
+    
+    static {
+        ChatModifier.INSTANCE = new ChatModifier();
     }
-
-    public
-    enum Suffix {
-        NONE,
-        Quantum
-
+    
+    public enum Suffix
+    {
+        Quantum++;
     }
 }
