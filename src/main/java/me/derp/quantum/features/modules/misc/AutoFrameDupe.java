371c6578ec4f9f2317bff19fdbc295c451be843e
MrBubblegum <mezoozoghby@gmail.com>
2022-09-27 17:22:09 +0300

    leux/hispania frameduper (skidded from experium :nerd:)

--- src/main/java/me/derp/quantum/features/modules/misc/AutoFrameDupe.java
+++ src/main/java/me/derp/quantum/features/modules/misc/AutoFrameDupe.java
@@ -4,63 +4,59 @@ import me.derp.quantum.features.modules.Module;
 import me.derp.quantum.features.setting.Setting;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItemFrame;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemShulkerBox;
 import net.minecraft.util.EnumHand;
 
-public class AutoFrameDupe
-extends Module {
-    public Setting<Integer> turns;
-    public Setting<Integer> ticks;
-    public Setting<Integer> range;
-    public Setting<Boolean> shulkersonly;
-    private int timeout_ticks;
+public class AutoFrameDupe extends Module {
+    private final Setting<Boolean> shulkersonly  = this.register(new Setting<Boolean>("ShulkersOnly", true));
+    private final Setting<Integer> range  = this.register(new Setting<Integer>("Range", 5, 0, 6));
+    private final Setting<Integer> turns  = this.register(new Setting<Integer>("Turns", 1, 0, 5));
+    private final Setting<Integer> ticks  = this.register(new Setting<Integer>("Ticks", 10, 1, 20));
+    private int timeoutTicks = 0;
 
     public AutoFrameDupe() {
-        super("FrameDupe", "6b dupe.", Module.Category.MISC, true, false, false);
-        this.shulkersonly = this.register(new Setting<Boolean>("Shulkers Only", true));
-        this.range = this.register(new Setting<Integer>("Range", 5, 0, 6));
-        this.turns = this.register(new Setting<Integer>("Turns", 1, 0, 3));
-        this.ticks = this.register(new Setting<Integer>("Ticks", 10, 1, 20));
-        this.timeout_ticks = 0;
-    }
-
-    private int getShulkerSlot() {
-        int n = -1;
-        for (int i = 0; i < 9; ++i) {
-            Item item = AutoFrameDupe.mc.player.inventory.getStackInSlot(i).getItem();
-            if (!(item instanceof ItemShulkerBox)) continue;
-            n = i;
-        }
-        return n;
+        super("AutoFrameDupe", "For 6b6t.org and 8b8t.me and more!", Module.Category.MISC, true, false ,false);
     }
 
     @Override
     public void onUpdate() {
-        if (AutoFrameDupe.mc.player != null && AutoFrameDupe.mc.world != null) {
-            int n;
-            if (this.shulkersonly.getValue().booleanValue() && (n = this.getShulkerSlot()) != -1) {
-                AutoFrameDupe.mc.player.inventory.currentItem = n;
+        if (mc.player != null && mc.world != null) {
+            if (shulkersonly.getValue()) {
+                int shulkerSlot = getShulkerSlot();
+                if (shulkerSlot != -1) {
+                    mc.player.inventory.currentItem = shulkerSlot;
+                }
             }
-            for (Entity entity : AutoFrameDupe.mc.world.loadedEntityList) {
-                if (!(entity instanceof EntityItemFrame) || !(AutoFrameDupe.mc.player.getDistance(entity) <= (float)this.range.getValue().intValue())) continue;
-                if (this.timeout_ticks >= this.ticks.getValue()) {
-                    if (((EntityItemFrame)entity).getDisplayedItem().getItem() == Items.AIR && !AutoFrameDupe.mc.player.getHeldItemMainhand().isEmpty) {
-                        AutoFrameDupe.mc.playerController.interactWithEntity((EntityPlayer)AutoFrameDupe.mc.player, entity, EnumHand.MAIN_HAND);
-                    }
-                    if (((EntityItemFrame)entity).getDisplayedItem().getItem() != Items.AIR) {
-                        for (int i = 0; i < this.turns.getValue(); ++i) {
-                            AutoFrameDupe.mc.playerController.interactWithEntity((EntityPlayer)AutoFrameDupe.mc.player, entity, EnumHand.MAIN_HAND);
+            for (Entity frame : mc.world.loadedEntityList) {
+                if (frame instanceof EntityItemFrame) {
+                    if (mc.player.getDistance(frame) <= range.getValue()) {
+                        if (timeoutTicks >= ticks.getValue()) {
+                            if (((EntityItemFrame) frame).getDisplayedItem().getItem() == Items.AIR && !mc.player.getHeldItemMainhand().isEmpty) {
+                                mc.playerController.interactWithEntity(mc.player, frame, EnumHand.MAIN_HAND);
+                            }
+                            if (((EntityItemFrame) frame).getDisplayedItem().getItem() != Items.AIR) {
+                                for (int i = 0; i < turns.getValue(); i++) {
+                                    mc.playerController.interactWithEntity(mc.player, frame, EnumHand.MAIN_HAND);
+                                }
+                                mc.playerController.attackEntity(mc.player, frame);
+                                timeoutTicks = 0;
+                            }
                         }
-                        AutoFrameDupe.mc.playerController.attackEntity((EntityPlayer)AutoFrameDupe.mc.player, entity);
-                        this.timeout_ticks = 0;
+                        ++timeoutTicks;
                     }
                 }
-                ++this.timeout_ticks;
             }
         }
     }
-}
 
+    private int getShulkerSlot() {
+        int shulkerSlot = -1;
+        for (int i = 0; i < 9; i++) {
+            Item item = mc.player.inventory.getStackInSlot(i).getItem();
+            if (item instanceof ItemShulkerBox) shulkerSlot = i;
+        }
+        return shulkerSlot;
+    }
+}
